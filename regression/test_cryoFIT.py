e1fe3fd1f21c75cc32e9703fb98442b8f2e0ac9e
Doonam in 13inch lanl laptop <doonam.kim@gmail.com>
2017-11-30 17:31:11 -0700

    fixed trjconv path

--- regression/test_cryoFIT.py
+++ regression/test_cryoFIT.py
@@ -8,7 +8,7 @@ import libtbx.phil.command_line
 from subprocess import check_output
 import libtbx.load_env
 
-cryo_fit_repository_dir = libtbx.env.dist_path("cryoFIT")
+cryoFIT_repository_dir = libtbx.env.dist_path("cryoFIT")
 
 def run ():
     # running this test_cryoFIT is not recommended to be ran at /Users/doonam/bin/phenix-dev-2747/modules/cryo_fit to avoid \
@@ -21,11 +21,11 @@ def run ():
         print "remove former result file"
         os.remove("minimized_c_term_renamed_by_resnum_oc.gro")
         
-    command_string = "cp " + cryo_fit_repository_dir + "/steps/2_clean_gro/*rename*.py ."
+    command_string = "cp " + cryoFIT_repository_dir + "/steps/2_clean_gro/*rename*.py ."
     print "\tcommand: ", command_string
     libtbx.easy_run.fully_buffered(command_string)
   
-    command_string = "cp " + cryo_fit_repository_dir + "/regression/input_file/minimized.gro ."
+    command_string = "cp " + cryoFIT_repository_dir + "/regression/input_file/minimized.gro ."
     print "\tcommand: ", command_string
     libtbx.easy_run.fully_buffered(command_string)
     
@@ -35,7 +35,7 @@ def run ():
     if (os.path.isfile("minimized_c_term_renamed_by_resnum_oc.gro")):
         print "OK\n"
     else:
-        print "regression for cryo_fit didn't run successfully, exit now.\n"
+        print "regression for cryoFIT didn't run successfully, exit now.\n"
         exit(1)
 
 if __name__=="__main__":
