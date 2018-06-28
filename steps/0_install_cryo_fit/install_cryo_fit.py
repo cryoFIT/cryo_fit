1af9fcf757ccee76162513560adecb4abe7a5f09
Doonam in 13inch lanl laptop <doonam.kim@gmail.com>
2018-06-28 16:58:40 -0600

    install is possible even without ~/src and ~/bin

--- steps/0_install_cryo_fit/install_cryo_fit.py
+++ steps/0_install_cryo_fit/install_cryo_fit.py
@@ -21,34 +21,10 @@ import shutil # for rmdir
 cryo_fit_repository_dir = libtbx.env.dist_path("cryo_fit")
 print "cryo_fit_repository_dir:", cryo_fit_repository_dir
 
-
-'''  
-# this is needed to import all common functions
-path = check_output(["which", "phenix.cryo_fit"])
-splited = path.split("/")
-
-# (works) /Users/doonam/bin/phenix-1.13rc1-2961/modules/cryo_fit
-# (works) /Applications/phenix-dev-3120/modules/cryo_fit
-command_path = ''
-for i in range(len(splited)-3):
-  command_path = command_path + splited[i] + "/"
-  
-command_path = command_path + "modules/cryo_fit/" # capital letter does matter, so I'm uniting into cryo_fit
-print "command_path:",command_path
-
-common_functions_path = command_path + "common_functions/"
-sys.path.insert(0, common_functions_path)
-
-from common_functions import  * # (sometimes) ImportError: No module named libtbx at doonam's newest personal macbookpro
-'''
-
-#'''
 common_functions_path = cryo_fit_repository_dir + "/common_functions/"
 sys.path.insert(0, common_functions_path)
 print "common_functions_path:",common_functions_path
 from common_functions import  * # (sometimes) ImportError: No module named libtbx at doonam's newest personal macbookpro
-#'''
-
 
 
 def clean ():
