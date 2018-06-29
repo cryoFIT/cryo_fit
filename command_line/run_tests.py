38aa361f673f99c144afab97b4957f39827c4742
Doonam in 13inch lanl laptop <doonam.kim@gmail.com>
2018-06-29 10:11:35 -0600

    now regression tests run in any folder

--- command_line/run_tests.py
+++ command_line/run_tests.py
@@ -14,56 +14,88 @@ cryo_fit_repository_dir = libtbx.env.dist_path("cryo_fit")
 
 if (__name__ == "__main__") :
 
-  # added by Nigel? temporarily disabled
-  assert len(os.listdir(os.getcwd()))==0, 'run in an empty directory'
-
-# Locate phenix executable
-  print "This cryo_fit.run executable comes from ", cryo_fit_repository_dir
-
-# copy input files to a current folder (although it may take longer time by copying these files, it is more organized \
-# with respect to development in the long term)
-
-  pdb_file_name = 'tRNA_tutorial.pdb'
-  map_file_name = 'tRNA_tutorial.map'
-
-  # added by Nigel? temporarily disabled since on Doonam's macbook pro can't run this
-  '''
-  shutil.copyfile(os.path.join(cryo_fit_repository_dir,
-                               'tutorial_input_files',
-                               pdb_file_name), pdb_file_name)
-  shutil.copyfile(os.path.join(cryo_fit_repository_dir,
-                               'tutorial_input_files',
-                               sit_file_name), sit_file_name)
-  '''
-
-  pdb_file_name_w_path = os.path.join(cryo_fit_repository_dir,
-                               'tutorial_input_files',
-                               pdb_file_name)
-
-  map_file_name_w_path = os.path.join(cryo_fit_repository_dir,
-                               'tutorial_input_files',
-                               map_file_name)
-
-  #command_string = "phenix.cryo_fit %(pdb_file_name)s %(map_file_name)s" % locals()
-  command_string = "phenix.cryo_fit %(pdb_file_name_w_path)s %(map_file_name_w_path)s devel=True" % locals()
-
-# Start the simplest testing
-  print "command that will be executed: ", command_string
-  print "(for your information) this run_tests took 2 minutes on Doonam's macbook pro"
-  print '\n ~> %s\n' % command_string
+    # added by Nigel so that this test runs in a clear path,, but,,,,???
+    assert len(os.listdir(os.getcwd()))==1, 'run in an empty directory'
+    print "os.listdir(os.getcwd()):",os.listdir(os.getcwd())
+    print "len(os.listdir(os.getcwd())):",len(os.listdir(os.getcwd()))
+    
+    
+    # Locate phenix executable
+    print "This cryo_fit.run executable comes from ", cryo_fit_repository_dir
+    
+    splited = cryo_fit_repository_dir.split("/")
+    regression_path = ''
+    for i in range(len(splited)-1):
+      regression_path = regression_path + splited[i] + "/"
 
 
-  # as of 06/28/2018, below printout on the screen didn't print on Doonam's mac screen
-  # so temporarily disabled
-  '''
-  rc = libtbx.easy_run.go(command=command_string)
-  print '*'*80
-  for line in rc.stdout_lines:
-    print line
-
-  print '*'*80
-  print rc.stderr_lines
-  '''
-
-  # temporarily use this instead
-  libtbx.easy_run.call(command=command_string)
+    ############# test 1, simple biomolecule all steps ###############
+    regression_path_1 = os.path.join(regression_path, 'phenix_regression/cryo_fit/emd_8249')
+    print "regression_path_1:", regression_path_1
+    os.chdir(regression_path_1)
+    
+    command_string = "python tst.py" % locals()
+    libtbx.easy_run.call(command=command_string)
+  
+    
+    ############# test 2, tutorial tRNA, each steps ###############
+    regression_path_2 = os.path.join(regression_path, 'phenix_regression/cryo_fit/tRNA')
+    print "regression_path_2:", regression_path_2
+    os.chdir(regression_path_2)
+    
+    for i in range (1,9):
+        command_string = "python tst_step_" + str(i) + ".py"
+        print "command_string:", command_string
+        libtbx.easy_run.call(command=command_string)
+        
+    command_string = "python tst_step_final.py"
+    print "command_string:", command_string
+    libtbx.easy_run.call(command=command_string)
+  
+  
+    ##########  don't run this test 3, tutorial_tRNA for all steps, since it takes 2 minutes #####
+    ########## tutorial_tRNA for each steps run individually anyway ##########
+    '''
+    pdb_file_name = 'tRNA_tutorial.pdb'
+    map_file_name = 'tRNA_tutorial.map'
+  
+    # added by Nigel? temporarily disabled since on Doonam's macbook pro can't run this
+    
+    #shutil.copyfile(os.path.join(cryo_fit_repository_dir,
+    #                             'tutorial_input_files',
+    #                             pdb_file_name), pdb_file_name)
+    #shutil.copyfile(os.path.join(cryo_fit_repository_dir,
+    #                             'tutorial_input_files',
+    #                             sit_file_name), sit_file_name)
+  
+    pdb_file_name_w_path = os.path.join(cryo_fit_repository_dir,
+                                 'tutorial_input_files',
+                                 pdb_file_name)
+  
+    map_file_name_w_path = os.path.join(cryo_fit_repository_dir,
+                                 'tutorial_input_files',
+                                 map_file_name)
+  
+    #command_string = "phenix.cryo_fit %(pdb_file_name)s %(map_file_name)s" % locals()
+    command_string = "phenix.cryo_fit %(pdb_file_name_w_path)s %(map_file_name_w_path)s devel=True" % locals()
+  
+    print "command that will be executed: ", command_string
+    print "(for your information) this run_tests took 2 minutes on Doonam's macbook pro"
+    print '\n ~> %s\n' % command_string
+  
+  
+    # as of 06/28/2018, below printout on the screen didn't print on Doonam's mac screen
+    # so temporarily disabled
+    
+    # rc = libtbx.easy_run.go(command=command_string)
+    # print '*'*80
+    # for line in rc.stdout_lines:
+    #   print line
+    # 
+    # print '*'*80
+    # print rc.stderr_lines
+  
+    # temporarily use this instead
+    libtbx.easy_run.call(command=command_string)
+    '''
+    
