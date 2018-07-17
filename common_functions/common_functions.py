aba9da9aa10b261f8447b8ba44fafcd25011c5d5
Doonam in 13inch lanl laptop <doonam.kim@gmail.com>
2018-07-17 11:11:06 -0600

    more map weight bumping for 2762

--- common_functions/common_functions.py
+++ common_functions/common_functions.py
@@ -155,7 +155,7 @@ def check_whether_cc_has_been_increased(logfile, cc_record):
   print "\t\tNumber of cc increase in the last ",step_number_for_judging," steps: ",cc_has_been_increased
   print "\t\tNumber of cc decrease in the last ",step_number_for_judging," steps: ",cc_has_been_decreased
 
-  if (cc_has_been_decreased > cc_has_been_increased*1.2):
+  if (cc_has_been_decreased > cc_has_been_increased):
     msg=(
     '\n\t\tcc tends to decrease over the last ' + str(step_number_for_judging) + ' steps.\n'
     
