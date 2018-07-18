77b998b596a9c85d86aaee5ad54dd5877dbbf547
Doonam in 13inch lanl laptop <doonam.kim@gmail.com>
2018-07-18 14:57:23 -0600

    confirmed molprobity error with fastest one, w tRNA, cryo_fitted pdb file is ok

--- steps/10_after_cryo_fit/clean_pdb_for_realspace_refine_molprobity.py
+++ steps/10_after_cryo_fit/clean_pdb_for_realspace_refine_molprobity.py
@@ -5,8 +5,8 @@ import glob, os, sys
 
 def clean_main(input_pdb_file_name):
   print "\n\t\tFor realspace_refine, remove CRYST line to avoid error \"Crystal symmetry mismatch between different files\""
-  print "\t\t\t\t     U instead of RU to avoid error"
-  print "\t\t\t\t     add element at the end of each line"
+  print "\t\t\t\t      U instead of RU to avoid error"
+  print "\t\t\t\t      add element at the end of each line"
   print "\n\t\tFor molprobity, change OC1 and OC2"
 
   output_pdb_file_name = clean (input_pdb_file_name) 
@@ -36,10 +36,7 @@ def clean(input_pdb_file_name):
     element = line[13:14]
     atom = line[13:16]
     
-    if ((CRYST_MODEL == "CRYST") or (CRYST_MODEL == "MODEL")): # remove CRYST for real_space_refine, remove MODEL for molprobity
-      print "\t\t\t omitted ", line
-      continue
-    elif (ENDMDL == "ENDMDL"): # remove for molprobity
+    if ((CRYST_MODEL == "CRYST") or (CRYST_MODEL == "MODEL") or (ENDMDL == "ENDMDL")): # remove CRYST for real_space_refine, remove MODEL for molprobity
       print "\t\t\t omitted ", line
       continue
     elif atom == "OC2":
@@ -48,7 +45,7 @@ def clean(input_pdb_file_name):
     elif (line[0:4] != "ATOM") and (line[0:6] != "HETATM"):
       f_out.write(line)
     else: # most cases
-      if (element == "H"): # for debug molprobity
+      if (element == "H"):
         continue
       new_line = line[:75] + "  " + element + line[79:] + "\n"
       new_line = new_line[:17] + new_line[17:20].replace(' RA', ' A ') + new_line[20:] #residue = line[17:20]
